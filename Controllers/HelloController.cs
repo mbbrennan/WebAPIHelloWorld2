a456cf73faeee33451421b539bbda64842a703a4
Maureen Brennan <mbbrennan@gmail.com>
2020-09-26 13:28:08 -0600

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIHelloWorld2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Well, hello! It it me your looking for?";
+        }
+    }
+}
